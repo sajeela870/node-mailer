30a83fbb55d64bc23653973d2491b62ea9d0f0f1
sajeela870 <164522012+sajeela870@users.noreply.github.com>
2024-06-17 00:03:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+async function sendEmail() {
+  
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: 'krishnapriya18claysys@gmail.com', 
+        subject: 'Hello',
+        text: 'Hello Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survivedLoincluding versions of Lorem Ipsum.'
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Email sent: ' + info.response);
+    });
+}
+
+sendEmail();
